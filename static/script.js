de5780aa84651e41ada5fb1b2560e05905d6bedd
Joon Park <JoonPark2017@u.northwestern.edu>
2017-03-17 03:51:13 -0500

    Add loading bar, disable submit when loading

--- static/script.js
+++ static/script.js
@@ -1,6 +1,22 @@
 $('#file-upload').submit(function(event) {
     event.preventDefault();
 
+    $('#results').html(`
+        <div class="progress">
+            <div
+                class="progress-bar progress-bar-striped active"
+                role="progressbar"
+                aria-valuenow="45"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style="width: 100%"
+            >
+            </div>
+        </div>
+    `);
+
+    $('#submit').attr('disabled', 'disabled');
+
     const data = new FormData($(this)[0]);
     $.ajax({
         url: 'upload',
@@ -9,6 +25,8 @@ $('#file-upload').submit(function(event) {
         contentType: false,
         processData: false,
         success: function(data) {
+            $('#results').empty();
+
             // Display image and audio file
             const image = document.createElement('img');
             $(image).attr('src', data.image);
@@ -21,6 +39,7 @@ $('#file-upload').submit(function(event) {
             $(audio).append(audio_source);
 
             $('#results').append(image, audio);
+            $('#submit').prop('disabled', false);
         }
     });
 });
